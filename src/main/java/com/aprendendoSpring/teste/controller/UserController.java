7752b1c57657c43014aac116a6e031ca176364b3
EliasBoaventura <elias4144@hotmail.com>
2024-01-27 15:07:37 -0300

    criado a entidade user e feito o teste no controller

---
+++ src/main/java/com/aprendendoSpring/teste/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.aprendendoSpring.teste.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import entities.User;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1l, "elias", "elias4144@hotmail.com", "9999999", "12345");
+
+		return ResponseEntity.ok().body(u);
+	}
+
+}
